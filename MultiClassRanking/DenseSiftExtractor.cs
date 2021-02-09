3426e9b84bea7efcce3099f73b59c9683d102dec
Shapour Nemati <shapour.nemati@studio.unibo.it>
2021-02-09 18:52:23 +0100

    feat: dense SIFT extractor

---
+++ MultiClassRanking/DenseSiftExtractor.cs
@@ -0,0 +1,47 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.XFeatures2D;
+using System;
+
+namespace PerExemplarMultiLabelImageClassification.MultiClassRanking
+{
+    class DenseSiftExtractor
+    {
+
+        private int spacing = 4;
+
+        public DenseSiftExtractor()
+        {
+        }
+
+        public IOutputArray ComputeDescriptor(Image<Bgr, byte> img)
+        {
+            SIFT sift = new SIFT();
+
+            
+            int pointsSize = 
+                (int) Math.Ceiling((double)img.Width / spacing) 
+                * 
+                (int) Math.Ceiling((double)img.Height / spacing);
+
+            int index = 0;
+            MKeyPoint[] points = new MKeyPoint[pointsSize];
+            for (int i = 0; i < img.Width; i += spacing)
+            {
+                for (int j = 0; j < img.Height; j += spacing)
+                {
+                    MKeyPoint p = new MKeyPoint();
+                    p.Point = new System.Drawing.PointF(i, j);
+                    p.Size = 8;
+                    points.SetValue(p, index++);
+                }
+            }
+
+            Emgu.CV.Util.VectorOfKeyPoint keyPoints = new Emgu.CV.Util.VectorOfKeyPoint(points);
+            IOutputArray descriptors = new Mat();
+            sift.Compute(img, keyPoints, descriptors);
+            return descriptors;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using PerExemplarMultiLabelImageClassification.MultiClassRanking;
 
 namespace PerExemplarMultiLabelImageClassification
 {
@@ -15,9 +19,19 @@ namespace PerExemplarMultiLabelImageClassification
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MultiClassRankingForm());
+            // Application.EnableVisualStyles();
+            // Application.SetCompatibleTextRenderingDefault(false);
+            // Application.Run(new MultiClassRankingForm());
+            TestDenseSiftExctracotr();
+        }
+
+        static void TestDenseSiftExctracotr()
+        {
+            Image<Bgr, byte> img = new Image<Bgr, byte>("E:\\UNI\\VISIONE\\Lab\\02 - CBIR system-20191016\\CeramicheFaenza\\Test\\0018_-_piatto_palmetta_40cm_2.jpg");
+            DenseSiftExtractor extractor = new DenseSiftExtractor();
+            Mat descriptor = (Mat)extractor.ComputeDescriptor(img);
+            Console.WriteLine(descriptor.GetValueRange().Max + " " + descriptor.GetValueRange().Min);
+            Console.ReadLine();
         }
     }
 }
