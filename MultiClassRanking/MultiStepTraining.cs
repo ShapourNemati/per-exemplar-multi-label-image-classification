1eb0c5fcffff8cbbfacd441da41075762903a678
Shapour Nemati <shapour.nemati@studio.unibo.it>
2021-02-14 10:53:53 +0100

    feat: skeletn for multi step training class

---
+++ MultiClassRanking/MultiStepTraining.cs
@@ -0,0 +1,65 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerExemplarMultiLabelImageClassification.MultiClassRanking
+{
+    class MultiStepTraining
+    {
+
+        public MultiStepTraining()
+        {
+
+        }
+
+        public void ComputeDescriptor(Image<Bgr, byte> img)
+        {
+
+        }
+
+        public void SaveDescriptorsToFile(string filename)
+        {
+
+        }
+
+        public void LoadDescriptorsFromFile(string filename)
+        {
+
+        }
+
+        public void ComputeCodeBook()
+        {
+
+        }
+
+        public void SaveCodeBookToFile(string filename)
+        {
+
+        }
+
+        public void LoadCodeBookFromFile(string filename)
+        {
+
+        }
+
+        public void TrainModel(Mat descriptor, string className)
+        {
+
+        }
+
+        public void SaveModelToFile(string filename)
+        {
+
+        }
+
+        public void LoadModelFromFile(string filename)
+        {
+
+        }
+
+    }
+}
